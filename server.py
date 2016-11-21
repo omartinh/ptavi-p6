5752685e6764801168995a3e5aad26f23e0c17e4
Oscar Martin Herrera <omartin@gamma39.aulas.gsyc.urjc.es>
2016-11-21 11:52:22 +0100

    servidor finalizado con pep8

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ import sys
 import os
 
 
-
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
@@ -17,30 +16,33 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion" + b"\r\n")
+        # self.wfile.write(b"Hemos recibido tu peticion" + b"\r\n")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            client_message = line.decode('utf-8').split()           
+            client_m = line.decode('utf-8').split()
             if not line:
                 break
-            if client_message[1].split(':')[0] != 'sip' or client_message[2] != 'SIP/2.0':
+            # Si la peticion esta mal formada
+            if client_m[1].split(':')[0] != 'sip' or client_m[2] != 'SIP/2.0':
                 print("El cliente nos manda " + line.decode('utf-8'))
                 self.wfile.write(b"SIP/2.0 400 Bad Request \r\n")
-            if client_message[0] == 'INVITE':
+            # Tratamos los metodos INVITE,ACK,BYE
+            if client_m[0] == 'INVITE':
                 print("El cliente nos manda " + line.decode('utf-8'))
                 self.wfile.write(b"SIP/2.0 100 Trying\r\n" +
-                                b"SIP/2.0 180 Ring\r\n" +
-                                b"SIP/2.0 200 OK\r\n")
-            elif client_message[0] == 'ACK':
+                                 b"SIP/2.0 180 Ring\r\n" +
+                                 b"SIP/2.0 200 OK\r\n")
+            elif client_m[0] == 'ACK':
                 print("El cliente nos manda " + line.decode('utf-8'))
                 aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + Audio_file
                 print("Vamos a ejecutar: " + aEjecutar)
                 os.system(aEjecutar)
-            elif client_message[0] == 'BYE':
+            elif client_m[0] == 'BYE':
                 print("El cliente nos manda " + line.decode('utf-8'))
                 self.wfile.write(b"SIP/2.0 200 OK\r\n")
-            elif not client_message[0] in ['ACK', 'INVITE', 'BYE']:
+            # Tratamos que los metodos esten bien
+            elif not client_m[0] in ['ACK', 'INVITE', 'BYE']:
                 print("Metodo erroneo: " + client_message[0])
                 self.wfile.write(b"SIP/2.0 405 Method Not Allowed \r\n")
 
